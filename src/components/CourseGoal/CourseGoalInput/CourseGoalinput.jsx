63e160b7c40a11e798d94f648ab004646632c9ca
Saddam Hafiz Alif <mr.alif.dev@gmail.com>
2023-10-03 23:16:46 +0600

    added form design

--- src/components/CourseGoal/CourseGoalInput/CourseGoalinput.jsx
+++ src/components/CourseGoal/CourseGoalInput/CourseGoalinput.jsx
@@ -1,20 +1,20 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import "./CourseGoalinput.css"
+import "./CourseGoalinput.css";
 import Button from "../../UI/Button";
 
 const CourseGoalInput = (props) => {
-    return (
-        <div>
-            <form action="">
-            <div className="input-wrap">
-                <label>Type Your Goal</label>
-                <input type="text" />
-            </div>
-            <Button type="submit">Add Goal</Button>
-        </form>
+  return (
+    <div className="form-wrap">
+      <form action="" className="form">
+        <div className="input-wrap">
+          <label>Type Your Goal</label>
+          <input type="text" />
         </div>
-    )
-}
+        <Button type="submit">Add Goal</Button>
+      </form>
+    </div>
+  );
+};
 
-export default CourseGoalInput
+export default CourseGoalInput;
