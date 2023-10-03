9f6d0e838b04e6d952862ad746ddf710160e576b
Saddam Hafiz Alif <mr.alif.dev@gmail.com>
2023-10-03 23:38:11 +0600

    added invalid input style

--- src/components/CourseGoal/CourseGoalInput/CourseGoalinput.jsx
+++ src/components/CourseGoal/CourseGoalInput/CourseGoalinput.jsx
@@ -4,12 +4,35 @@ import "./CourseGoalinput.css";
 import Button from "../../UI/Button";
 
 const CourseGoalInput = (props) => {
+  const [isValid, setIsValid] = useState(true);
+  const [enteredGoal, setEnteredGoal] = useState("");
+
+  const onChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredGoal(event.target.value);
+  };
+
+  const onSUbmitHandler = (event) => {
+    event.preventDefault();
+    console.log("Hi")
+
+    if (enteredGoal.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    console.log(enteredGoal);
+
+    setEnteredGoal('')
+  };
+
   return (
     <div className="form-wrap">
-      <form action="" className="form">
-        <div className="input-wrap">
+      <form action="" onSubmit={onSUbmitHandler} className="form">
+        <div className={`input-wrap ${!isValid ? "invalid" : ''}`}>
           <label>Type Your Goal</label>
-          <input type="text" />
+          <input type="text" value={enteredGoal} onChange={onChangeHandler} />
         </div>
         <Button type="submit">Add Goal</Button>
       </form>
